8fdb928e4395efe2939b66a0564892c05b6ec4bc
sunny <sunny@sunnys-MacBook-Pro.local>
2025-06-30 21:53:37 -0400

    fixed

--- loadTest/tests/laodtest.spec.ts
+++ loadTest/tests/laodtest.spec.ts
@@ -7,6 +7,13 @@ test('load testing', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Votre Magasin' }).fill('Magasin 1');
     await page.locator('#caisse').fill('Caisse 1');
     await page.getByRole('button', { name: 'Se connecter' }).click();
+    let i = 0;
+    while (i < 500) {
+        await page.getByRole('textbox', { name: 'Rechercher un produit...' }).fill('d');
+        await page.getByRole('textbox', { name: 'Rechercher un produit...' }).press('Enter');
+        await page.getByRole('link', { name: 'Produits' }).click();
+        i++;
+    }
 
 });
 
@@ -16,7 +23,11 @@ test('load testing 2', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Votre mot de passe' }).fill('password');
     await page.getByRole('button', { name: 'Se connecter' }).click();
     await page.getByRole('link', { name: 'Rapports' }).click();
-    await page.getByRole('button', { name: 'Générer' }).click();
+    let i = 0;
+    while (i < 500) {
+        await page.getByRole('button', { name: 'Générer' }).click();
+        i++;
+    }
 
 })
 
@@ -26,15 +37,21 @@ test('load testing 3', async ({ page }) => {
     await page.getByRole('textbox', { name: 'Votre mot de passe' }).fill('password');
     await page.getByRole('button', { name: 'Se connecter' }).click();
     await page.getByRole('link', { name: 'Produits' }).click();
-    await page.getByRole('textbox', { name: 'Rechercher un produit...' }).fill('s');
-    await page.getByRole('textbox', { name: 'Rechercher un produit...' }).press('Enter');
-    await page.locator('.btn-primary').first().click();
-    await page.locator('input[name="nom"]').fill('Cff');
-    await page.getByRole('spinbutton').fill('12.00');
-    await page.locator('#editPriceForm input[name="description"]').fill('new descp');
-    page.once('dialog', dialog => {
-        console.log(`Dialog message: ${dialog.message()}`);
-        dialog.dismiss().catch(() => { });
-    });
-    await page.getByRole('button', { name: 'Mettre à jour' }).click();
+    let i = 0;
+    while (i < 500) {
+        await page.getByRole('textbox', { name: 'Rechercher un produit...' }).clear();
+        await page.getByRole('textbox', { name: 'Rechercher un produit...' }).fill('s');
+        await page.getByRole('textbox', { name: 'Rechercher un produit...' }).press('Enter');
+        await page.locator('.mdf-btn').first().click();
+        await page.locator('input[name="nom"]').fill('s');
+        await page.getByRole('spinbutton').fill('12.00');
+        await page.locator('#editPriceForm input[name="description"]').fill('new descp');
+        page.once('dialog', dialog => {
+            console.log(`Dialog message: ${dialog.message()}`);
+            dialog.dismiss().catch(() => { });
+        });
+        await page.getByRole('button', { name: 'Mettre à jour' }).click();
+
+        i++;
+    }
 })
